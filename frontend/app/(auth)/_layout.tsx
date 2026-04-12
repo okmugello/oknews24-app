daa942bb3710bfd880ed2549ad3946848c5e695e
Lutherbli <lutherbli@gmail.com>
2026-04-12 12:27:03 +0200

    Fix navigazione password dimenticata e layout

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -5,6 +5,7 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }} initialRouteName="login">
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
+      <Stack.Screen name="forgot-password" />
     </Stack>
   );
 }
